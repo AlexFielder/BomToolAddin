ec40f2fbdd6afea46fc3fa733a5736f556076344
Alex Fielder <alex.fielder@gmail.com>
2015-03-24 09:10:57 +0000

    Commit of copied-from-iLogic code

---
+++ BomToolAddin/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("BomToolAddin")> 
+<Assembly: AssemblyDescription("BomToolAddin")> 
+<Assembly: AssemblyCompany("Autodesk, Inc.")> 
+<Assembly: AssemblyProduct("Autodesk Inventor")> 
+<Assembly: AssemblyCopyright("Copyright ?2012 Autodesk, Inc.")> 
+<Assembly: AssemblyTrademark("")> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1a0649c5-9c89-4657-98dc-2e6e49c31134")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.*")> 
